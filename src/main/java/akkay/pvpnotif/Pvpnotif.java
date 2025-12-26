dd891510561ed58becbbc0a950fb966179e495c9
akkay <ak.kayleeforu@gmail.com>
2025-12-26 05:45:47 +0200

    Third

--- src/main/java/akkay/pvpnotif/Pvpnotif.java
+++ src/main/java/akkay/pvpnotif/Pvpnotif.java
@@ -5,8 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.player.Player;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 import java.util.*;
 
 class EffectTriggered {
@@ -32,10 +32,11 @@ class EffectTriggered {
 }
 
 public class Pvpnotif implements ModInitializer {
-	public static final String MOD_ID = "pvpnotif";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	// public static final String MOD_ID = "pvpnotif";
+	// public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	private final HashMap<String, EffectTriggered> trackedEffects = new HashMap<>();
 	private final Set<String> pvpEffects = new HashSet<>();
+	private final List<String> effectsToDelete = new LinkedList<>();
 
 	// Variables
 	private final int threshold = 400; // 20 seconds
@@ -46,30 +47,39 @@ public class Pvpnotif implements ModInitializer {
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			Player player = client.player;
 			if (player == null) return;
-			getEffect(player);
-			for (int i = 0; i < trackedEffects.size(); i++) {
-				for (String effect : trackedEffects.keySet()) {
-					EffectTriggered effectTriggered = trackedEffects.get(effect);
-					if ((!effectTriggered.isEffectTriggered()) && effectTriggered.getDuration() < threshold) {
-						effectTriggered.setEffectTriggered(true);
-						System.out.println(effect + " will end in " + threshold/20 + " seconds.");
-						continue;
-					}
-                    if (effectTriggered.getDuration() == 0 ) {
-						// TODO Save the effects to delete to the list and delete them after the loop
-						trackedEffects.remove(effect);
-					}
+			Set<String> existingEffects = getEffect(player);
+
+			// Check each saved effect
+			for (String effect : trackedEffects.keySet()) {
+				EffectTriggered effectTriggered = trackedEffects.get(effect);
+				// Check if the effect is still on player
+				if (!existingEffects.contains(effect)) effectsToDelete.add(effect);
+
+				// Check if the notification was given and if the duration hit the threshold
+				if ((!effectTriggered.isEffectTriggered()) && effectTriggered.getDuration() <= threshold) {
+					effectTriggered.setEffectTriggered(true);
+					System.out.println(effect + " will end in " + threshold/20 + " seconds.");
+				}
+				// Check if the effect was renewed
+				else if (effectTriggered.isEffectTriggered() && effectTriggered.getDuration() > threshold) {
+					effectTriggered.setEffectTriggered(false);
 				}
 			}
+			// Delete each effect that was removed from the player
+			for (String effectToDelete : effectsToDelete) {
+				trackedEffects.remove(effectToDelete);
+			}
+			effectsToDelete.clear();
 		});
 	}
 
-	void getEffect(Player player) {
+	Set<String> getEffect(Player player) {
+		Set <String> existingEffects = new HashSet<>();
 		for (MobEffectInstance effect : player.getActiveEffects()) {
 			for (String pvpEffect : pvpEffects) {
 				String effectID = effect.getDescriptionId();
-
 				if (effectID.equals(pvpEffect)) {
+					existingEffects.add(effectID);
 					if (!trackedEffects.containsKey(effectID)) {
 						EffectTriggered effectTriggered = new EffectTriggered(effect.getDuration());
 						trackedEffects.put(effectID, effectTriggered);
@@ -78,8 +88,8 @@ public class Pvpnotif implements ModInitializer {
 					break;
 				}
 			}
-
 		}
+		return existingEffects;
 	}
 
 	void initializeEffects() {
