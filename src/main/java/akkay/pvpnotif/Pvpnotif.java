dd8a525ac56b171ae9bc39438434b0e6ceebf69f
akkay <ak.kayleeforu@gmail.com>
2025-12-26 05:45:47 +0200

    Config file creation

--- src/main/java/akkay/pvpnotif/Pvpnotif.java
+++ src/main/java/akkay/pvpnotif/Pvpnotif.java
@@ -1,5 +1,6 @@
 package akkay.pvpnotif;
 
+import com.google.gson.Gson;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
@@ -7,6 +8,8 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.player.Player;
 // import org.slf4j.Logger;
 // import org.slf4j.LoggerFactory;
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
 class EffectTriggered {
@@ -34,9 +37,22 @@ class EffectTriggered {
 public class Pvpnotif implements ModInitializer {
 	// public static final String MOD_ID = "pvpnotif";
 	// public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	private final HashMap<String, EffectTriggered> trackedEffects = new HashMap<>();
+
+    // Set of the effects in minecraft form that are going to be checked on the player
 	private final Set<String> pvpEffects = new HashSet<>();
+
+    // Map of the effects that were detected on a player and are going to be checked each tick
+    private final HashMap<String, EffectTriggered> trackedEffects = new HashMap<>();
+
+    // List that has all the effects that will be removed from checking, because invalid
 	private final List<String> effectsToDelete = new LinkedList<>();
+
+    // File Object
+    private final File configFile = new File("config/pvpnotif.json");
+
+    // Gson Object
+    // TODO continue
+    Gson gson = new Gson();
 
 	// Variables
 	private final int threshold = 400; // 20 seconds
@@ -44,6 +60,20 @@ public class Pvpnotif implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		initializeEffects();
+        // TODO Change the text output, maybe leave only the init message
+        try {
+            if (!configFile.exists()) {
+                System.out.println("File does not exist, it will be created");
+                createConfigFile();
+            }
+            else System.out.println("File already exists");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("PvP Notifications has been initialized");
+        }
+
+
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			Player player = client.player;
 			if (player == null) return;
@@ -54,14 +84,15 @@ public class Pvpnotif implements ModInitializer {
 				EffectTriggered effectTriggered = trackedEffects.get(effect);
 				// Check if the effect is still on player
 				if (!existingEffects.contains(effect)) effectsToDelete.add(effect);
+                int timeLeft = effectTriggered.getDuration();
 
-				// Check if the notification was given and if the duration hit the threshold
-				if ((!effectTriggered.isEffectTriggered()) && effectTriggered.getDuration() <= threshold) {
+                // Check if the notification was given and if the duration hit the threshold
+				if ((!effectTriggered.isEffectTriggered()) && timeLeft <= threshold) {
 					effectTriggered.setEffectTriggered(true);
-					System.out.println(effect + " will end in " + threshold/20 + " seconds.");
+					System.out.println(effect + " will end in " + timeLeft/20 + " seconds.");
 				}
 				// Check if the effect was renewed
-				else if (effectTriggered.isEffectTriggered() && effectTriggered.getDuration() > threshold) {
+				else if (effectTriggered.isEffectTriggered() && timeLeft > threshold) {
 					effectTriggered.setEffectTriggered(false);
 				}
 			}
@@ -101,5 +132,10 @@ public class Pvpnotif implements ModInitializer {
 		pvpEffects.add("effect.minecraft.fire_resistance");
 		pvpEffects.add("effect.minecraft.invisibility");
 		pvpEffects.add("effect.minecraft.slow_falling");
+        pvpEffects.add("effect.minecraft.absorption");
 	}
+
+    void createConfigFile() throws IOException {
+        configFile.createNewFile();
+    }
 }
